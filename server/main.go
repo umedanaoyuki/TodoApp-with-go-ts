bd11ebdd7ba659e39595424ac1db57f78673d7c4
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-17 22:58:21 +0900

    Patch追加

--- server/main.go
+++ server/main.go
@@ -38,6 +38,22 @@ func main() {
 		return c.JSON(todos)
 	})
 
+	app.Patch("api/todos/:id/done", func (c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+
+		for i, t := range todos {
+			 if t.ID == id {
+				todos[i].Done = true
+				break
+			 }
+		}
+		
+		return c.JSON(todos)
+	})
 	
 	log.Fatal(app.Listen(":4000"))
 
